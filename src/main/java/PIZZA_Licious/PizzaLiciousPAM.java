bf5e742143dd91767ab4430511b897989b019125
kietnnn <kietnn@gmail.com>
2025-11-14 08:04:19 +0700

    a

--- src/main/java/PIZZA_Licious/PizzaLiciousPAM.java
+++ src/main/java/PIZZA_Licious/PizzaLiciousPAM.java
@@ -2,7 +2,7 @@ package PIZZA_Licious;
 
 import PIZZA_Licious.userinterface.UserInterface;
 
-public class PizzaLiciousApp {
+public class PizzaLiciousPAM {
     public static void main(String[] args) {
         UserInterface userInterface = new UserInterface();
         userInterface.start();
